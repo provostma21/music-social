7c94eeb8fe35b2d63664048511ccdb7542f8b26e
Matthew M. Provost <provostmat@gmail.com>
2023-10-13 20:27:38 -0400

    added link and state change for onclick for Home page on first navbar link

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,16 @@
 import "./Style/Navbar.css";
 
-export default function Navbar() {
+export default function Navbar({ currentPage, handlePageChange }) {
   return (
     <ul className="nav navbar">
       <li className="nav-item">
-        <a href=""></a>
+        <a
+          href="Home"
+          onClick={() => handlePageChange("Home")}
+          className={currentPage === "Home" ? "nav-link active" : "nav-link"}
+        >
+          Home
+        </a>
       </li>
       <li className="nav-item">
         <a href=""></a>
