323aa4457421f600450a3d7d0b60a6e7a2d30390
Matthew M. Provost <provostmat@gmail.com>
2023-12-21 23:11:10 -0500

    added div to Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,9 @@
 import "./Home.css";
 
 export default function Home() {
-  return <div className="Home-Container"></div>;
+  return (
+    <div className="Home-Container">
+      <div></div>
+    </div>
+  );
 }
